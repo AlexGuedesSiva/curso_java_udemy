f6ad7c82a8593122729f5d887482dc79b0260042
ALEX WILSON GUEDES DA SILVA <alex__guedes@hotmail.com>
2022-11-21 10:43:57 -0300

    ConversorCelsiusParaFahrenheit

---
+++ ConversaoCelsiusParaFhenheit.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class ConversaoCelsiusParaFhenheit {
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.printf("Informe a Temperatura Em Celsius: ");
+		double num1 = entrada.nextDouble();
+
+		double resultado = (num1 * 1.8) + 32;
+
+		System.out.printf("Valor Temperatura em Fahrenheit: %.2f", resultado);
+
+		entrada.close();
+
+	}
+
+}
